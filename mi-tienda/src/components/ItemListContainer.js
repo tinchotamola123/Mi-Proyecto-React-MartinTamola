1148c54d59117aab95341436a8d37063f1d57ca8
Martin Tamola <tinchotamola123@gmail.com>
2022-09-19 21:14:10 -0300

    PreEntrega1 Tamola Martin

--- mi-tienda/src/components/ItemListContainer.js
+++ mi-tienda/src/components/ItemListContainer.js
@@ -2,8 +2,6 @@ import './ItemListContainer.css'
 import Container from 'react-bootstrap/Container'
 
 const ItemListContainer = ({ greeting }) => {
-    //TODO mostrar la props greeting
-    //TODO ubicarlo dentro del Apps.js y abajo del NavBar.js
     return (
         <Container className='mt-5'> 
             <h3 className="greeting">{greeting}</h3>
