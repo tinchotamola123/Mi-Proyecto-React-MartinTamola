e9535a8e132a494f79d9c6504f7a1c1f5062d102
Martin Tamola <tinchotamola123@gmail.com>
2022-09-26 16:55:35 -0300

    PreEntrega1 Tamola Martin

--- mi-tienda/src/components/Item.js
+++ mi-tienda/src/components/Item.js
@@ -3,17 +3,46 @@ import Card from 'react-bootstrap/Card';
 
 const Item = ({product}) => {
     return ( 
-        <Card style={{ width: '18rem' }}>
+        <>
+        {[
+            'Secondary',
+        ].map((variant) => (
+            <Card
+                bg={variant.toLowerCase()}
+                key={variant}
+                text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+                style={{ width: '18rem' }}
+                className="mb-2"
+            >
+            <Card.Header><Card.Img variant="top" src="holder.js/100px180" /></Card.Header>
+            <Card.Body>
+                <Card.Title>{product.name}</Card.Title>
+                <Card.Text>{product.description}</Card.Text>
+                <Button variant="primary">Añadir al Carrito</Button>
+            </Card.Body>
+            </Card>
+        ))}
+        </>
+    );
+}
+
+export default Item;
+
+
+
+
+
+
+
+
+
+ {/* <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src="holder.js/100px180" />
                 <Card.Body>
                 <Card.Title>{product.name}</Card.Title>
                 <Card.Text>
                 {product.description}
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Button variant="primary">Añadir al Carrito</Button>
             </Card.Body>
-        </Card>
-    );
-}
-
-export default Item;
+        </Card> */}
